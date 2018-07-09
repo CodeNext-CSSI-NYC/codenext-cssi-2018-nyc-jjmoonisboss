c2fd3f4a2ac716665670a0f8a8c37204d30cd55b
JJMOON <jelyse.williams@csedge.org>
2018-07-09 16:36:48 -0400

    math exersises
    Quadrouple Yay

--- math-practice.js
+++ math-practice.js
@@ -9,15 +9,16 @@ const q = 60;
 const r = 25;
 
 // 1. Is 91 divisible by 13? (The answer should be either true or false)
-
+console.log = (91 % 13==0);
 // 2. What is the result when the q divided by r is rounded to the nearest integer?
+Math.round(q/r);
 
 // 3. Is the product of p and r greater than q squared? (The answer should be either true or false)
-
+console.log = (p * r > r ** 2 ==1);
 // 4. What is the remainder when the sum of q and r is divided by p?
-
+console.log = (q+r/p%);
 // 5. 27 squared is equal to what number raised to the third power?
-
+console.log = (27 ** 2 = 9 ** 3);
 // 6. What is the absolute value of p - q? (Find out how to do absolute value by searching online.)
 
 // 7. Find another math operation we haven't talked about yet on the MDN (Mozilla Developer Network) and try it out below.
