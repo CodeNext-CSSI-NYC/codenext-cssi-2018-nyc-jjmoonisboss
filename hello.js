dec7e5bab5d45904233ea6b77fafc37c242a6f64
JJMOON <jelyse.williams@csedge.org>
2018-07-10 10:37:11 -0400

    Now you can say hi and i got new packages
    Sextuple yay! (thats how you say 6 i guess)

--- hello.js
+++ hello.js
@@ -1,9 +1,25 @@
 // Author: FirstName LastName
-console.log("Hello World!");
-console.log("It's a beautiful day for programming, isn't it?");
-console.log("Hey! What's 1 + 1?");
-console.log("It's " + 1 + 1 + " of course!");
-console.log("No, it's " + (1 + 1) + ", silly goose!");
+var readline = require("readline-sync");
+let yourName = readline.question("What is your name :");
+console.log("Nice to meet you " + yourName + " !");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello World!");
+// console.log("It's a beautiful day for programming, isn't it?");
+// console.log("Hey! What's 1 + 1?");
+// console.log("It's " + 1 + 1 + " of course!");
+// console.log("No, it's " + (1 + 1) + ", silly goose!");
 // console.log("This isn't going to get printed, is it?");
 
 
@@ -21,15 +37,15 @@ console.log("No, it's " + (1 + 1) + ", silly goose!");
 // var num1 = 7 % 4;
 // console.log(num1);
 
-var num1 = 10 % 2;
-console.log(num1);
-
-var num1 =3;
-
-var num2 =52;
-
- var num3 = num2 % num1;
-
- num1 =num3 - 1;
-
-console.log(num1)
+// var num1 = 10 % 2;
+// console.log(num1);
+//
+// var num1 =3;
+//
+// var num2 =52;
+//
+//  var num3 = num2 % num1;
+//
+//  num1 =num3 - 1;
+//
+// console.log(num1)
