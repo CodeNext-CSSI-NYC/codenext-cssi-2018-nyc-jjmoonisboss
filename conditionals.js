4ce1d24f45c5f6335e64681c9853a01e4156f398
JJMOON <jelyse.williams@csedge.org>
2018-07-11 12:36:38 -0400

    started to play around with conditionals
    quattuordecuple yay!

--- conditionals.js
+++ conditionals.js
@@ -16,9 +16,9 @@
     console.log("You're answer "+"' " + num + "'"+  " is a positive number!!");
   }
 // 2. Ask the user to enter three numbers, and then print out the biggest number they entered.
-let  nums =readline.question("Please enter one number here: ");
-let doubleNums =readline.question("Please enter another number here: ");
-let tripleNums =readline.question("Please enter one more number here: ");
+let  nums =parseFloat(readline.question("Please enter one number here: "));
+let doubleNums =parseFloat(readline.question("Please enter another number here: "));
+let tripleNums =parseFloat(readline.question("Please enter one more number here: "));
 if (nums > doubleNums && nums > tripleNums){
   console.log("The max number out of " + nums + ", " + doubleNums  + " and "  + tripleNums + " is " + nums + "!");
 }
@@ -33,15 +33,43 @@ else {
 let eveOh= readline.question("Give me a number, it can be any number: ");
 if (eveOh %= 0) {
 console.log("Your number is an even number!!")}
+else if (evenOh == 0){
+  console.log("You're number isssss ZEROOOOOO ");
 else{
   console.log("Your number is an odd number")
 }
 
 // 4. Ask the user to enter a letter and tell them if it is a vowel or consonant
+// a = 0;
+// b = 1;
+// c = 2;
+// d = 3;
+let vowConst =readline.question("Enter a letter any letter (my fav is 'j' what's yours ?): ")
+if (vowConst == "a"||vowConst == "e"||vowConst =="i"||vowConst =="o"||vowConst =="u"){
+  console.log("Your letter is a vowellll!")
+}
+else if (vowConst == "y"){
+  console.log("your letter is a semi-vowel! coolio <3")
+}
+else if (vowConst =="j"){
+  console.log("10/10 consonant! best letter")
+}
+else if (vowConst !="a"||vowConst !="e"||vowConst !="i"||vowConst !="o"||vowConst !="u") {
+  console.log("your letter is a consonant")
+}
 
 // 5. Ask the user to enter a year, and tell them if it was a leap year.
 // (You can use Google if you're not sure when leap years happen.)
+console.log("******************************************************************")
+console.log("Welcome to the leap year discoverer!")
 let daYear = readline.question("Please enter a year, any year (yes even 2016 *dun dun dun*): ");
+if (daYear / 4){
+  console.log(daYear +  " is a leap year<3 ")
+}
+else {
+  console.log("Sorry " + daYear + " isn't a leap year </3")
+}
+console.log("******************************************************************")
 
 // 6. Ask the user if they have been a citizen for at least 9 years (yes or no), ask them their age, and ask if they live in New York state.
 // If they answer yes, their age is above 30, and yes, then tell them they can run for senate in NY. Otherwise, say that they cannot.
