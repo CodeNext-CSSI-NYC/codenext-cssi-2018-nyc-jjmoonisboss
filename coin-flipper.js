154490b28425731f9d8569bbc8f1e9bffe2474c3
JJMOON <jelyse.williams@csedge.org>
2018-07-11 12:58:06 -0400

    for the me one i just saved and im not finishied with coin flipper
     quindecuple yay

---
+++ coin-flipper.js
@@ -0,0 +1,12 @@
+//Jelyse Williams
+  var readline = require("readline-sync");
+  let num= readline.question("type a number, any number at alllll~ ")
+  let mun= Math.random();
+  if (mun < 0.5){
+    let result ="HEADS"
+  }
+  else {
+    let result= "tails"
+  }
+  console.log("You've got"+ mun + "You are")
+  if 
