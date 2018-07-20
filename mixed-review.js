049ca8279e79afee43dd21f382977dd9a6b5a3af
JJMOON <jelyse.williams@csedge.org>
2018-07-20 12:55:49 -0400

    downloaded modules and doing mixed review
    trigintuple yay!

--- mixed-review.js
+++ mixed-review.js
@@ -73,10 +73,10 @@ function firstTime(arr, num) {
     // arr[i].length;
 
     if (arr[i] == num) {
-      return true;
+      return i;
     }
   }
-  return false;
+
 
 }
 // If your function is correct, these should both print true.
@@ -95,144 +95,145 @@ function lastTime(arr, num) {
     if (target == num) {
       return i;
     }
-
-
   }
-  // If your function is correct, these should both print true.
-  console.log(lastTime([2, 3, 4, 2, 5, 3, 4], 4) == 6);
-  console.log(lastTime([2, 3, 4, 2, 5, 3, 4], 3) == 5);
+
+}
+
+// If your function is correct, these should both print true.
+console.log(lastTime([2, 3, 4, 2, 5, 3, 4], 4) == 6);
+console.log(lastTime([2, 3, 4, 2, 5, 3, 4], 3) == 5);
 
 
 
 
 
-  // 5. Use a for loop and multiplication to create a function that calculates
-  // exponents for you (and returns the answer).
-  // Do NOT use the exponent operation **, that defeats the whole point of the exercise.
-  function powers(base, exponent) {
-    let result = 1;
-    // Hint: For loop goes here. Each time through the loop, you should do some
-    // multiplication and store the answer back in result.
+// 5. Use a for loop and multiplication to create a function that calculates
+// exponents for you (and returns the answer).
+// Do NOT use the exponent operation **, that defeats the whole point of the exercise.
+function powers(base, exponent) {
+  let result = 1;
+  // Hint: For loop goes here. Each time through the loop, you should do some
+  // multiplication and store the answer back in result.
+}
+// If your function is correct, these should print true.
+// console.log(powers(2, 5) == 32);
+// console.log(powers(3, 4) == 81);
+// console.log(powers(17, 1) == 17);
+
+
+// 6. Write a function that takes in a string called word and returns true/false
+// whether or not the word starts with L.
+function startsWithL(word) {
+  //console.log(word.charAt(0));
+  if (word.charAt(0) == "L" || word.charAt(0) == "l") {
+    return true;
+  } else {
+    return false;
   }
-  // If your function is correct, these should print true.
-  // console.log(powers(2, 5) == 32);
-  // console.log(powers(3, 4) == 81);
-  // console.log(powers(17, 1) == 17);
+}
+// If your function is correct, these should print true.
+console.log(startsWithL("Liam") == true);
+console.log(startsWithL("orange") == false);
+console.log(startsWithL("lion") == true);
 
 
-  // 6. Write a function that takes in a string called word and returns true/false
-  // whether or not the word starts with L.
-  function startsWithL(word) {
-    //console.log(word.charAt(0));
-    if (word.charAt(0) == "L" || word.charAt(0) == "l") {
-      return true;
-    } else {
-      return false;
-    }
+
+// Make sure you have coped all of the words from
+// https://raw.githubusercontent.com/CodeNext-CSSI-NYC/CodeNext-CSSI-2018/master/words.txt
+// into a file called words.txt in the same folder as all of your other files.
+// The top of this file gives you an array called words which contains all the words
+// from that file.
+
+
+
+// 7. Write a function that takes in a word and returns true/false
+// whether or not that word is in the array of words.
+function inWords(word) {
+  //
+}
+// These should all print true when you are done.
+// console.log(inWords("star") == true);
+// console.log(inWords("starrrrr") == false);
+
+
+
+// 8. Write a function that takes in a letter and returns an array of
+// all of the words that start and end with that letter.
+function startAndEndWith(letter) {
+  //
+}
+// These should all print true when you are done.
+// console.log(startAndEndWith("y").length == 3);
+// console.log(startAndEndWith("y").includes("yearly"));
+// console.log(startAndEndWith("w").length == 7);
+// console.log(startAndEndWith("w").includes("workflow"));
+
+
+
+// 9. Write a function that takes in a number and returns the factorial of that number.
+// More info: https://www.mathsisfun.com/numbers/factorial.html
+// Use a for loop. Do not try to use the factorial symbol (!), it does not mean that in coding.
+function factorial(num) {
+  let product = 1;
+  for (var i = 1; i <= num; i++) {
+    num *= i;
   }
-  // If your function is correct, these should print true.
-  console.log(startsWithL("Liam") == true);
-  console.log(startsWithL("orange") == false);
-  console.log(startsWithL("lion") == true);
+  console.log(num);
+}
+// These should all print true when you are done.
+console.log(factorial(5) == 120);
+console.log(factorial(1) == 1);
 
 
 
-  // Make sure you have coped all of the words from
-  // https://raw.githubusercontent.com/CodeNext-CSSI-NYC/CodeNext-CSSI-2018/master/words.txt
-  // into a file called words.txt in the same folder as all of your other files.
-  // The top of this file gives you an array called words which contains all the words
-  // from that file.
-
-
-
-  // 7. Write a function that takes in a word and returns true/false
-  // whether or not that word is in the array of words.
-  function inWords(word) {
-    //
+// 10. Write a function that takes in a number and returns an array that adds that many
+// Fibonacci numbers to the starting array of [0, 1].
+// More info: https://www.mathsisfun.com/numbers/fibonacci-sequence.html
+// Use a for loop.
+// For example, if you call fibonacci(3), that means we want to add three numbers
+// onto that array, so the return value should be the array [0, 1, 1, 2, 3]
+function fibonacci(num) {
+  let arr = [0, 1]; // This is the starter array that you will add numbers to
+  // For loop here
+  for (var i = 0; i < num; i++) {
+    arr.length - 1;
+    let lastNum = arr[arr.length - 1];
+    let secondToLastNum = arr[arr.length - 2];
+    arr.push(lastNum + secondToLastNum);
   }
-  // These should all print true when you are done.
-  // console.log(inWords("star") == true);
-  // console.log(inWords("starrrrr") == false);
-
-
-
-  // 8. Write a function that takes in a letter and returns an array of
-  // all of the words that start and end with that letter.
-  function startAndEndWith(letter) {
-    //
-  }
-  // These should all print true when you are done.
-  // console.log(startAndEndWith("y").length == 3);
-  // console.log(startAndEndWith("y").includes("yearly"));
-  // console.log(startAndEndWith("w").length == 7);
-  // console.log(startAndEndWith("w").includes("workflow"));
-
-
-
-  // 9. Write a function that takes in a number and returns the factorial of that number.
-  // More info: https://www.mathsisfun.com/numbers/factorial.html
-  // Use a for loop. Do not try to use the factorial symbol (!), it does not mean that in coding.
-  function factorial(num) {
-    let product = 1;
-    for (var i = num - 1; i >= 1; i--) {
-      num *= i;
-    }
-    console.log(num);
-  }
-  // These should all print true when you are done.
-  console.log(factorial(5) == 120);
-  console.log(factorial(1) == 1);
-
-
-
-  // 10. Write a function that takes in a number and returns an array that adds that many
-  // Fibonacci numbers to the starting array of [0, 1].
-  // More info: https://www.mathsisfun.com/numbers/fibonacci-sequence.html
-  // Use a for loop.
-  // For example, if you call fibonacci(3), that means we want to add three numbers
-  // onto that array, so the return value should be the array [0, 1, 1, 2, 3]
-  function fibonacci(num) {
-    let arr = [0, 1]; // This is the starter array that you will add numbers to
-    // For loop here
-    for (var i = 0; i < num; i++) {
-      arr.length - 1;
-      let lastNum = arr[arr.length - 1];
-      let secondToLastNum = arr[arr.length - 2];
-      arr.push(lastNum + secondToLastNum);
-    }
-    console.log(arr);
-  }
-  // These tests should print true when you are done
-  console.log(fibonacci(5).pop() == 8);
-  console.log(fibonacci(8).pop() == 24);
+  return arr;
+}
+// These tests should print true when you are done
+console.log(fibonacci(5).pop() == 8);
+console.log(fibonacci(8).pop() == 34);
 
 
 
 
-  // 11. Write a function that takes in an object called coins (see example below) and
-  // returns the amount of money those coins represent in dollars.
-  function countMoney(coins) {
-    //   let cents = 0;
-    // const quarters = Math.floor(cents(25));
-    //  cents = cents % 25;
-  }
-  // Below is a test that calls this function with an object that four properties,
-  // each representing how many coins I have.
-  let myCoins = {
-    quarters: 2,
-    dimes: 3,
-    nickels: 4,
-    pennies: 5
-  };
-  // If your function is correct, this should print true.
-  // console.log(countMoney(myCoins) == 1.05);
+// 11. Write a function that takes in an object called coins (see example below) and
+// returns the amount of money those coins represent in dollars.
+function countMoney(coins) {
+  //   let cents = 0;
+  // const quarters = Math.floor(cents(25));
+  //  cents = cents % 25;
+}
+// Below is a test that calls this function with an object that four properties,
+// each representing how many coins I have.
+let myCoins = {
+  quarters: 2,
+  dimes: 3,
+  nickels: 4,
+  pennies: 5
+};
+// If your function is correct, this should print true.
+// console.log(countMoney(myCoins) == 1.05);
 
 
 
-  // 12. Write a function that keep asking the user to type in a number and stops
-  // as soon as they type in 24. Unlike the others, this function does not return anything.
-  function waitFor24() {
+// 12. Write a function that keep asking the user to type in a number and stops
+// as soon as they type in 24. Unlike the others, this function does not return anything.
+function waitFor24() {
 
-  }
-  // Call it and make sure it works
-  waitFor24();
+}
+// Call it and make sure it works
+waitFor24();
